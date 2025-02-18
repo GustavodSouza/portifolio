7b1a07b9fd6336b87cb00daa9ab0aba8b77a4f36
Gustavo de Souza de Lima <gustavo.delima@castgroup.com.br>
2025-02-18 15:32:06 -0300

    feat: Ajustes e adição de projeto.

---
+++ scripts/projetos.js
@@ -0,0 +1,27 @@
+export const projetos = [
+    {
+        titulo: "Chat App Vue 3",
+        github: "https://github.com/GustavodSouza/vue3-chat-app",
+        site: "https://vue3-chat.vercel.app",
+        img: "assets/meus-gastos2.png",
+        descricao: "Chat app desenvolvido utiliando Vue 3 com firebase.",
+        tecnologias: "Vue 3 - Javascript - Typescript - Scss - Bootstrap - Firebase",
+    },
+    {
+        titulo: "Meus Gastos",
+        github: "https://github.com/GustavodSouza/MeusGastos",
+        site: "https://meus-gastos.vercel.app/",
+        img: "assets/meus-gastos2.png",
+        descricao: "Projeto pessoal desenvolvido para controlar os gastos mensais. Neste projeto é possível registrar os gastos e fazer consultas obtendo o valor total por mês.",
+        tecnologias: "Angular - Javascript - Typescript - Scss - Material Design - Firebase - Pwa",
+    },
+    {
+        titulo: "Tasks React",
+        github: "https://github.com/GustavodSouza/Tasks-react.git",
+        site: "https://tasks-react-gustavo.vercel.app",
+        img: "assets/tasks-react.png",
+        descricao:
+        "Projeto realizado com a finalidade de aprender React. Nele é possível armazenar uma lista de tarefas, sendo possível adicionar, marcar como feita e excluir.",
+        tecnologias: "React - Javascript",
+    },
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,90 +1,150 @@
-let projetos = [
-    {
-      titulo: "Meus Gastos",
-      github: "https://github.com/GustavodSouza/MeusGastos",
-      site: "https://meus-gastos.vercel.app/",
-      img: "assets/meus-gastos2.png",
-      descricao: "Projeto pessoal desenvolvido para controlar os gastos mensais. Neste projeto é possível registrar os gastos e fazer consultas obtendo o valor total por mês.",
-      tecnologias: "Angular - Typescript - Scss - Firebase - Pwa",
-    },
-    {
-      titulo: "Tasks React",
-      github: "https://github.com/GustavodSouza/Tasks-react.git",
-      site: "https://tasks-react-gustavo.vercel.app",
-      img: "assets/tasks-react.png",
-      descricao:
-        "Projeto realizado com a finalidade de aprender React. Nele é possível armazenar uma lista de tarefas, sendo possível adicionar, marcar como feita e excluir.",
-      tecnologias: "React - Javascript",
-    },
-  ];
-  
-  let click = false;
-  let input = document.getElementById("clrinput");
-  const botaoVerMais = document.getElementById("ver-mais");
-  const card = document.getElementById("projetos-card");
-  
-  carregarProjetos = (filtrados) => {
-    const data = filtrados ? filtrados : projetos;
-  
-    botaoVerMais.style.display = data.length > 4 ? '' : 'none';
-  
-    if (data.length > 0) {
-      data.forEach((item, index) => {
-        const display = index > 3 ? "hidden" : "show"
-  
-        card.innerHTML += `
-          <div class="pt-5 col-md-12 col-sm-12 ${display} cards" style="border: 1px solid white; border-radius: 5px">
-              <div class="column">
-                  <p class="h4 mb-4">${item.titulo}</p>
-                  <p>${item.descricao}</p>
-                  <strong>${item.tecnologias}</strong>
-                  <div class="d-flex justify-content-center py-3 gap-4">
-                     <a data-toggle="tooltip" data-placement="top" title="Visualizar Repositório" class="buttons-card" href="${item.github}" target="_blank">
-                         Repositório
-                         <i class="fa-brands fa-github icon"></i>
-                     </a>
-                     <a data-toggle="tooltip" data-placement="top" title="Visualizar Projeto" class="buttons-card" href="${item.site}" target="_blank">
-                         Visitar projeto
-                         <i class="fa-solid fa-eye icon"></i>
-                     </a>
-                 </div>
-              </div>
-          </div>`;
-      });
-      const elems = document.querySelectorAll(".hidden");
-      for (const elem of elems) {
-        elem.style.display = "none";
-      }
-    } else {
-      card.innerHTML = '<strong>Nenhum projeto encontrado com esta tecnologia!</strong>';
-    }
-  };
-  
-  carregarProjetos();
-  
-  input.addEventListener("input", () => {
-    card.innerHTML = "";
-  
-    if (input.value) {
-      click = false;
-      const inputSearch = input.value.toLowerCase()
-      const filtrados = projetos.filter((item) => item.tecnologias.toLowerCase().match(inputSearch) || item.titulo.toLowerCase().match(inputSearch))
-      carregarProjetos(filtrados);
-    } else {
-      carregarProjetos();
-    }
-  });
-  
-  verMaisProjetos = () => {
-    click = !click;
-  
+import { projetos } from './projetos.js';
+
+let click = false;
+let input = document.getElementById("clrinput");
+
+const botaoVerMais = document.getElementById("ver-mais");
+const card = document.getElementById("projetos-card");
+const filtroTechs = document.getElementById("filtro-techs");
+
+// Função de renderização dos projetos
+function renderizarProjetos(filtrados) {
+  const data = filtrados ? filtrados : projetos;
+
+  botaoVerMais.style.display = data.length > 4 ? '' : 'none';
+
+  if (data.length > 0) {
+    card.innerHTML = '';
+
+    data.forEach((item, index) => {
+      const display = index > 3 ? "hidden" : "show"
+
+      card.innerHTML += `
+        <div class="pt-5 col-md-12 col-sm-12 ${display} cards" style="border: 1px solid white; border-radius: 5px">
+            <div class="column">
+                <p class="h4 mb-4">${item.titulo}</p>
+                <p>${item.descricao}</p>
+                <strong>${item.tecnologias}</strong>
+                <div class="d-flex justify-content-center py-3 gap-4">
+                    <a data-toggle="tooltip" data-placement="top" title="Visualizar Repositório" class="buttons-card" href="${item.github}" target="_blank">
+                        Repositório
+                        <i class="fa-brands fa-github icon"></i>
+                    </a>
+                    <a data-toggle="tooltip" data-placement="top" title="Visualizar Projeto" class="buttons-card" href="${item.site}" target="_blank">
+                        Visitar projeto
+                        <i class="fa-solid fa-eye icon"></i>
+                    </a>
+                </div>
+            </div>
+        </div>`;
+    });
     const elems = document.querySelectorAll(".hidden");
-  
     for (const elem of elems) {
-      elem.style.display = click ? "flex" : "none";
+      elem.style.display = "none";
     }
+  } else {
+    card.innerHTML = '<strong>Nenhum projeto encontrado com esta tecnologia!</strong>';
+  }
+}
+
+// Função para "ver mais" projetos
+function verMaisProjetos() {
+  click = !click;
+
+  const elems = document.querySelectorAll(".hidden");
+
+  for (const elem of elems) {
+    elem.style.display = click ? "flex" : "none";
+  }
+
+  botaoVerMais.textContent = click
+    ? "Ver menos"
+    : "Ver mais";
+}
+
+// Função para separar as tecnologias
+function separarTechs() {
+  let techsSeparadas = new Map();
+
+  projetos.forEach((tech) => {
+    const techsDesestructed = tech.tecnologias.split('-');
+
+    techsDesestructed.forEach((techDesestruct) => {
+      techsSeparadas.set(techDesestruct.trim(), techDesestruct.trim());
+    })
+  });
+
+  renderizarFiltrosCheckboxes(techsSeparadas);
+}
+
+// Função para renderizar os filtros de checkbox
+function renderizarFiltrosCheckboxes(techsSeparadas) {
+  for (const valor of techsSeparadas.values()) {
+    filtroTechs.innerHTML += `
+      <div class="form-check form-check-inline d-flex">
+        <input id="checkboxInpt" class="form-check-input checkbox" type="checkbox" id="techs${valor}" value="${ valor }">
+        <label class="form-check-label px-3" for="techs${valor}">${ valor }</label>
+      </div>
+    `;
+  }
+}
+
+// Função para filtrar os projetos
+function filtrarCheckbox() {
+  let techsSelecionadas = [];
+
+  document.querySelectorAll('.form-check-input:checked').forEach((checkbox) => {
+    techsSelecionadas.push(checkbox.value);
+  });
+
+  const filtrados = projetos.filter(proj =>
+    techsSelecionadas.some(tech => proj.tecnologias.includes(tech))
+  );
+
+  let result = []
+
+  result = filtrados.length ? filtrados : projetos;
+
+  renderizarProjetos(result);
+}
+
+// Função para limpar os checkboxes
+function limparCheckboxes() {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  checkboxes.forEach(checkbox => {
+    checkbox.checked = false;
+  });
+}
+
+// Adicionando o evento de input para o campo de busca
+input.addEventListener("input", () => {
+  card.innerHTML = "";
+
+  limparCheckboxes();
   
-    botaoVerMais.textContent = click
-      ? "Ver menos"
-      : "Ver mais";
-  };
+  if (input.value) {
+    click = false;
+    const inputSearch = input.value.toLowerCase()
+    const filtrados = projetos.filter((item) => item.tecnologias.toLowerCase().match(inputSearch) || item.titulo.toLowerCase().match(inputSearch))
+    renderizarProjetos(filtrados);
+  } else {
+    renderizarProjetos();
+  }
+});
+
+// Iniciando o processo
+renderizarProjetos();
+separarTechs();
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Seleciona todos os checkboxes com a classe 'checkbox'
+  let checkboxes = document.querySelectorAll('.checkbox');
+
+  // Itera sobre todos os checkboxes e adiciona um ouvinte de evento para o 'change'
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+      filtrarCheckbox();
+    });
+  });
+});
